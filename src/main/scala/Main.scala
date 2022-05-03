fb26272f4a40b1aac7c0a887b83cf1d5a8e23c23
Marcelo Iury <51893535+MarceloINS@users.noreply.github.com>
2022-05-03 14:57:59 -0300

    Projeções e simplificação de expressões

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -2,7 +2,7 @@
   val aux = new Auxiliar
   val linearAlgebra = new LinearAlgebra
   val transformacoes = new Transformacoes
-  
+
   val angle = 90
 
   val a = aux createMatriz (2,1)
@@ -52,4 +52,21 @@
   println()
   println("Matriz shearing Y")
   aux exibirMatriz (transformacoes shearingY (a, 3))
+  println()
+
+  //projeção
+  println("Matriz projection X2D")
+  aux exibirMatriz (transformacoes projectionX2D a)
+  println()
+  println("Matriz projection Y2D")
+  aux exibirMatriz (transformacoes projectionY2D a)
+  println()
+  println("Matriz projection X3D")
+  aux exibirMatriz (transformacoes projectionX3D b)
+  println()
+  println("Matriz projection Y3D")
+  aux exibirMatriz (transformacoes projectionY3D b)
+  println()
+  println("Matriz projection Z3D")
+  aux exibirMatriz (transformacoes projectionZ3D b)
   println()
--- src/main/scala/Transformacoes.scala
+++ src/main/scala/Transformacoes.scala
@@ -6,57 +6,83 @@ class Transformacoes:
   //rotação
   def rotation2D(vector: Matriz, angle: Double) =
     if aux isVector vector then
-      linearAlgebra.dot(matrizesTransformacoes.rotation2D(angle), vector)
+      linearAlgebra dot (matrizesTransformacoes rotation2D angle, vector)
     else throw Exception("O primeiro parâmetro precisa ser um Vetor")
 
   def rotationX3D(vector: Matriz, angle: Double) =
     if aux isVector vector then
-      linearAlgebra.dot(matrizesTransformacoes.rotationX3D(angle), vector)
+      linearAlgebra dot (matrizesTransformacoes rotationX3D angle, vector)
     else throw Exception("O primeiro parâmetro precisa ser um Vetor")
 
   def rotationY3D(vector: Matriz, angle: Double) =
     if aux isVector vector then
-      linearAlgebra.dot(matrizesTransformacoes.rotationY3D(angle), vector)
+      linearAlgebra dot (matrizesTransformacoes rotationY3D angle, vector)
     else throw Exception("O primeiro parâmetro precisa ser um Vetor")
 
   def rotationZ3D(vector: Matriz, angle: Double) =
     if aux isVector vector then
-      linearAlgebra.dot(matrizesTransformacoes.rotationZ3D(angle), vector)
+      linearAlgebra dot (matrizesTransformacoes rotationZ3D angle, vector)
     else throw Exception("O primeiro parâmetro precisa ser um Vetor")
 
   //cisalhamento
   def shearingX(vector: Matriz, a: Double) =
     if aux isVector vector then
-      linearAlgebra.dot(matrizesTransformacoes.shearingX(a), vector)
+      linearAlgebra dot (matrizesTransformacoes shearingX a, vector)
     else throw Exception("O primeiro parâmetro precisa ser um Vetor")
 
   def shearingY(vector: Matriz, a: Double) =
     if aux isVector vector then
-      linearAlgebra.dot(matrizesTransformacoes.shearingY(a), vector)
+      linearAlgebra dot (matrizesTransformacoes shearingY a, vector)
     else throw Exception("O primeiro parâmetro precisa ser um Vetor")
 
   //reflex
   def reflexX2D(vector: Matriz) =
     if aux isVector vector then
-      linearAlgebra.dot(matrizesTransformacoes.reflexX2D, vector)
+      linearAlgebra dot (matrizesTransformacoes.reflexX2D, vector)
     else throw Exception("O primeiro parâmetro precisa ser um Vetor")
 
   def reflexY2D(vector: Matriz) =
     if aux isVector vector then
-      linearAlgebra.dot(matrizesTransformacoes.reflexY2D, vector)
+      linearAlgebra dot (matrizesTransformacoes.reflexY2D, vector)
     else throw Exception("O primeiro parâmetro precisa ser um Vetor")
 
   def reflexX3D(vector: Matriz) =
     if aux isVector vector then
-      linearAlgebra.dot(matrizesTransformacoes.reflexX3D, vector)
+      linearAlgebra dot (matrizesTransformacoes.reflexX3D, vector)
     else throw Exception("O primeiro parâmetro precisa ser um Vetor")
 
   def reflexY3D(vector: Matriz) =
     if aux isVector vector then
-      linearAlgebra.dot(matrizesTransformacoes.reflexY3D, vector)
+      linearAlgebra dot (matrizesTransformacoes.reflexY3D, vector)
     else throw Exception("O primeiro parâmetro precisa ser um Vetor")
 
   def reflexZ3D(vector: Matriz) =
     if aux isVector vector then
-      linearAlgebra.dot(matrizesTransformacoes.reflexZ3D, vector)
+      linearAlgebra dot (matrizesTransformacoes.reflexZ3D, vector)
+    else throw Exception("O primeiro parâmetro precisa ser um Vetor")
+
+  //projeção
+  def projectionX2D(vector: Matriz) =
+    if aux isVector vector then
+      Matriz(2, 1, Array(Array(vector.elementos(0)(0)), Array(0.0)))
+    else throw Exception("O primeiro parâmetro precisa ser um Vetor")
+
+  def projectionY2D(vector: Matriz) =
+    if aux isVector vector then
+      Matriz(2, 1, Array(Array(0.0), Array(vector.elementos(1)(0))))
+    else throw Exception("O primeiro parâmetro precisa ser um Vetor")
+
+  def projectionX3D(vector: Matriz) =
+    if aux isVector vector then
+      Matriz(3, 1, Array(Array(vector.elementos(0)(0)), Array(0.0), Array(0.0)))
+    else throw Exception("O primeiro parâmetro precisa ser um Vetor")
+
+  def projectionY3D(vector: Matriz) =
+    if aux isVector vector then
+      Matriz(3, 1, Array(Array(0.0), Array(vector.elementos(1)(0)), Array(0.0)))
+    else throw Exception("O primeiro parâmetro precisa ser um Vetor")
+
+  def projectionZ3D(vector: Matriz) =
+    if aux isVector vector then
+      Matriz(3, 1, Array(Array(0.0), Array(0.0), Array(vector.elementos(2)(0))))
     else throw Exception("O primeiro parâmetro precisa ser um Vetor")
