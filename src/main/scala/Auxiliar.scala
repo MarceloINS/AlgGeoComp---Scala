da87a7cab28eb23ca79dab36d64700d0b45b4d3b
Marcelo Iury <51893535+MarceloINS@users.noreply.github.com>
2022-06-14 18:19:08 -0300

    Tortura

--- src/main/scala/Auxiliar.scala
+++ src/main/scala/Auxiliar.scala
@@ -6,7 +6,7 @@ class Auxiliar:
   def createMatriz(linhas: Int, colunas: Int) = new Matriz(linhas, colunas, createElements(linhas, colunas))
 
   def createElements(linhas: Int, colunas: Int): Array[Array[Double]] =
-    var start = Array.fill(linhas)(Array.fill(colunas)(1.0))
+    val start = Array.fill(linhas)(Array.fill(colunas)(1.0))
     for(i <- 0 until linhas)
       for(j <- 0 until colunas)
         start(i)(j) = r.between(1.0, 10.0).round
@@ -18,9 +18,64 @@ class Auxiliar:
         if j == (a.colunas-1) then
           println(a.elementos(i)(j))
         else print(s"${a.elementos(i)(j)} ")))
-    
+
   def isVector(vector: Matriz) =
     if(vector.colunas != 1) then
       false
     else
       true
+
+  def getMatrizAdjacencia =
+    val lin = 10
+    val col = 10
+    val elem = Array.fill(lin)(Array.fill(col)(0.0))
+    elem(0)(1) = 1.0
+    elem(5)(1) = 1.0
+    elem(5)(2) = 1.0
+    elem(5)(3) = 1.0
+    elem(0)(4) = 1.0
+    elem(1)(4) = 1.0
+    elem(2)(4) = 1.0
+    elem(5)(4) = 1.0
+    elem(7)(4) = 1.0
+    elem(3)(5) = 1.0
+    elem(8)(5) = 1.0
+    elem(9)(5) = 1.0
+    elem(3)(6) = 1.0
+    elem(0)(7) = 1.0
+    elem(4)(7) = 1.0
+    elem(5)(7) = 1.0
+    elem(5)(9) = 1.0
+    elem(6)(9) = 1.0
+    Matriz(lin, col, elem)
+
+  def somaLin(a: Matriz) =
+    val res = Array.fill(a.linhas)(Array.fill(1)(0.0))
+    for(i <- 0 until a.linhas)
+      for(j <- 0 until a.linhas)
+        res(i)(0) = res(i)(0) + a.elementos(j)(i)
+    Matriz(a.linhas, 1, res)
+
+  def somaCol(a: Matriz) =
+    val res = Array.fill(a.linhas)(Array.fill(1)(0.0))
+    for(i <- 0 until a.linhas)
+      for(j <- 0 until a.linhas)
+        res(i)(0) = res(i)(0) + a.elementos(i)(j)
+    Matriz(a.linhas, 1, res)
+
+  def mod(a: Matriz) =
+    var acumulate = 0.0
+    for(i <- 0 until a.linhas)
+        acumulate = acumulate + a.elementos(i)(0) * a.elementos(i)(0) //caguei pro ^
+    acumulate
+
+  def div(a: Matriz, b: Double) =
+    val res = Array.fill(a.linhas)(Array.fill(1)(0.0))
+    for(i <- 0 until a.linhas)
+      res(i)(0) = a.elementos(i)(0)/Math.sqrt(b)
+    Matriz(a.linhas,1, res)
+
+//  def rankUp(a: Matriz) =
+//    val res = Array.fill(a.linhas)(Array.fill(1)(0.0))
+//      for(i <- 2 until a.linhas)
+//        var key = a.elementos(i)(0)
--- src/main/scala/MatrizesTransformacoes.scala
+++ src/main/scala/MatrizesTransformacoes.scala
@@ -17,4 +17,4 @@ class MatrizesTransformacoes:
   def rotation2D(angle: Double) = new Matriz(2,2, Array(Array(cos(angle),-(sin(angle))), Array(sin(angle),cos(angle))))
   def rotationX3D(angle: Double) = new Matriz(3,3, Array(Array(1.0, 0.0, 0.0), Array(0.0, cos(angle), -(sin(angle))), Array(0.0, sin(angle), cos(angle))))
   def rotationY3D(angle: Double) = new Matriz(3,3, Array(Array(cos(angle), 0.0, -(sin(angle))), Array(0.0,1.0,0.0), Array(sin(angle), 0.0, cos(angle))))
-  def rotationZ3D(angle: Double) = new Matriz(3,3, Array(Array(cos(angle),-(sin(angle)),0.0), Array(0.0,1.0,0.0), Array(0.0,0.0,-1.0)))
+  def rotationZ3D(angle: Double) = new Matriz(3, 3, Array(Array(cos(angle),-(sin(angle)),0.0), Array(0.0,1.0,0.0), Array(0.0,0.0,-1.0)))
